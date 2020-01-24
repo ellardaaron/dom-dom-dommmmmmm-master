a6abdb4e9e9ab55bc6a0399dd07f868be0c1f99a
Aaron Ellard <student@InnovateBhams-MacBook-Pro.local>
2020-01-24 13:47:03 -0600

    finished

--- app.js
+++ app.js
@@ -23,17 +23,28 @@ document.addEventListener("DOMContentLoaded", function () {
         idDisplay.classList.add('id-display')
         idDisplay.innerText = num
 
+        var colors = ["#85e6c5", "#51b9c6", "#6087c6", "#4d59ae", "#401a8e"];
+        square.addEventListener("click", function (e) {
+            e.target.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+
+        })
+
+        square.addEventListener("dblclick", function (e) {
+            if (square.id % 2 == 0) {
+                if (document.contains(square.nextSibling)) {
+                    square.nextSibling.remove()
+                } else {
+                    alert('There is no square to delete')
+                }
+            } else {
+                if (document.contains(square.previousSibling)) {
+                    square.previousSibling.remove()
+                } else {
+                    alert('There is no square to delete')
+                }
+            }
+        })
 
     })
-
-
-
-
-
-
-
-
-
-
 
 })
